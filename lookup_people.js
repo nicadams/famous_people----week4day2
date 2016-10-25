2e5a103bb64c78d9f6df078d9dbbd06e6c33d41a
Nic Adams <nic.adams@hotmail.com>
2016-10-25 21:29:43 +0000

    final

--- lookup_people.js
+++ lookup_people.js
@@ -24,12 +24,21 @@ client.connect((err) => {
     values: [input]
     }, (err, result) => {
       var person = result.rows[0];
+      var day = person.birthdate.getDate();
+      if (day < 10) {
+        day = "0" + day;
+      }
+      var month = person.birthdate.getMonth() + 1;
+      if (month < 10) {
+        month = "0" + month;
+      }
+      var year = person.birthdate.getFullYear();
 
       if (err) {
         return console.error("error running query", err);
       }
       console.log(`Found 1 person(s) by the name '${input}':`);
-      console.log(`- ${person.id}: ${person.first_name} ${person.last_name}, born '${person.birthdate}'`);
+      console.log(`- ${person.id}: ${person.first_name} ${person.last_name}, born '${year}-${month}-${day}'`);
       client.end();
   });
 });
